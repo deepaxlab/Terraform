f360cfd089ee5324e0c62b3ad822cc9252472ffb
root <root@vmd117195.contaboserver.net>
2024-05-12 17:21:50 +0200

    correction in main.tf file

--- Basic_ec2_Project/main.tf
+++ Basic_ec2_Project/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-    region = "eu-central-1"             # region of instance, in this case its frankfurt
+    region = "eu-central-1"                   # region of instance, in this case its frankfurt
 }
 
 resource "aws_instance" "Terraform" {
-    ami = "ami-098c93bd9d119c051"       # Ubuntu ami selected 
-    instance_type = "t2.micro"          # t2.micro selected
-    key_name = "aws_linux"              # uses my aws_linux.pem key for log in 
+    ami = "ami-098c93bd9d119c051"             # Ubuntu ami selected 
+    instance_type = "t2.micro"                # t2.micro selected
+    key_name = "aws_linux"                    # uses my aws_linux.pem key for log in 
     tags = { 
-        Name = "Terraform"              # creates instance name to Terraform 
+        Name = "Terraform"                    # creates instance name to Terraform 
         }
 }
 
 output "public-ip-address" {
-  value = aws_instance.Terraform.public_ip
+  value = aws_instance.Terraform.public_ip    # provides ip-add of the instance after creation 
 }
