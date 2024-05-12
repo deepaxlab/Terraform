8fb5499bd2afe2df136f989ab9b8284186f20c8d
root <root@vmd117195.contaboserver.net>
2024-05-12 16:49:31 +0200

    corrected the main.tf file

--- Basic_ec2_Project/main.tf
+++ Basic_ec2_Project/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "eu-central-1"  
+    region = "eu-central-1"             # region of instance, in this case its frankfurt
 }
 
-resource "aws_instance" "ec2" {
-    ami = "ami-098c93bd9d119c051"
-    instance_type = "t2.micro"
-    key_name = "aws_linux"
-    tags = {
-        Name = Terraform
-    } 
+resource "aws_instance" "Terraform" {
+    ami = "ami-098c93bd9d119c051"       # Ubuntu ami selected 
+    instance_type = "t2.micro"          # t2.micro selected
+    key_name = "aws_linux"              # uses my aws_linux.pem key for log in 
+    tags = { 
+        Name = "Terraform"              # Creates instance name to Terraform 
+        }
 }
